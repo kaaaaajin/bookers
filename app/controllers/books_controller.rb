9999c0ea3e66851433a0520993c453cfd0485181
kaaaaajin <119267797+kaaaaajin@users.noreply.github.com>
2022-12-10 11:24:29 +0000

    [add3]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: "Book was successfully created" 
     else
      @books = Book.all
      render :index
@@ -25,9 +25,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated"
+    else
+    render :edit
+    end
   end
   
   def destroy
